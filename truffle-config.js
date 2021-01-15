f9b8acbf172563720cdaf0eaabb0f67dfd03af65
Juan <pront23@gmail.com>
2021-01-15 13:07:08 +0900

    fixed spelling errors

--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   networks: {
     development: {
-      host:"127.0.0.1",
+      host: '127.0.0.1',
       port: 7545,
-      network_id: "5777" // Match any network id
-    }
+      network_id: '5777', // Match any network id
+    },
   },
   // Configure your compilers
   solc: {
-    optimizmer: {
+    optimizer: {
       enabled: true,
-      runs: 200
-    }
-  }
-}
+      runs: 200,
+    },
+  },
+};
